f781129ece090b1f80f45b8c9548434ffeb59ee8
James Hannah <jhannah8124@gmail.com>
2016-02-12 11:22:21 -0800

    project initialization

---
+++ desktop/src/com/sharplabs/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sharplabs.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sharplabs.game.SharpSkates;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SharpSkates(), config);
+	}
+}
