6b7bcf510501f5a2b08023d494d22560f38a2bee
James Hannah <jhannah8124@gmail.com>
2016-02-12 20:46:18 -0800

    skater class now takes size argument

---
+++ core/src/com/sharplabs/game/Skater.java
@@ -0,0 +1,66 @@
+package com.sharplabs.game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.Array;
+
+public class Skater {
+	Array<Animation> sprites;
+	Texture img;
+	float showTime;
+
+	public enum Direction {
+		Right,
+		Left,
+		Up,
+		Down,
+		Wipeout
+	}
+
+	public Skater(final Texture image, int size) {
+		// store the image
+		img = image;
+
+		sprites = new Array();
+
+		TextureRegion tmp1 = new TextureRegion(img, 0, 0, size, size);
+		TextureRegion tmp2 = new TextureRegion(img, 0, size, size, size);
+		TextureRegion tmp3 = new TextureRegion(img, size, 0, size, size);
+		TextureRegion tmp4 = new TextureRegion(img, size, size, size, size);
+		TextureRegion tmp5 = new TextureRegion(img, size*2, 0, size, size);
+		TextureRegion tmp6 = new TextureRegion(img, size*2, size, size, size);
+		TextureRegion tmp7 = new TextureRegion(img, size*3, 0, size, size);
+		TextureRegion tmp8 = new TextureRegion(img, size*3, size, size, size);
+
+		// set up right animation
+		sprites.add(new Animation(0.6f,
+					new TextureRegion(img, 0, 0, size, size),
+					new TextureRegion(img, 0, size, size, size)));
+		// set up left animation
+		tmp1.flip(true, false);
+		tmp2.flip(true, false);
+		sprites.add(new Animation(0.6f, tmp1, tmp2));
+		// set up up animation
+		tmp3.flip(true, false);
+		sprites.add(new Animation(0.6f, new TextureRegion(img, size, 0, size, size), tmp3));
+		// set up down animation
+		tmp4.flip(true, false);
+		sprites.add(new Animation(0.6f, new TextureRegion(img, size, size, size, size), tmp4));
+		// set up wipeout animation
+		tmp5.flip(false, true);
+		tmp6.flip(false, true);
+		tmp7.flip(false, true);
+		tmp8.flip(false, true);
+		sprites.add(new Animation(0.2f,
+					new TextureRegion(img, size*2, 0, size, size),
+					new TextureRegion(img, size*2, size, size, size),
+					new TextureRegion(img, size*3, 0, size, size),
+					new TextureRegion(img, size*3, size, size, size),
+					tmp5, tmp6, tmp7, tmp8);
+	}
+
+	public Animation direction(Direction dir) {
+		return sprites.get(dir.ordinal());
+	}
+}
